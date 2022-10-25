84ac69045e1cd620be31330b21043702c5617e0a
Aleksey <raven.lexa@yandex.ru>
2022-10-25 21:53:20 +0300

    Examples008_IntroMethod

--- Examples008_IntroMethod/Program.cs
+++ Examples008_IntroMethod/Program.cs
@@ -19,7 +19,8 @@ int c3 = 39;
 //int max2 = Max(a2, b2, c2);
 //int max3 = Max(a3, b3, c3);
 //int max = Max(max1, max2, max3);
-int max = Max(Max(a1, b1, c1), 
+int max = Max(
+    Max(a1, b1, c1), 
     Max(a2, b2, c2), 
     Max(a3, b3, c3));
 System.Console.WriteLine(max);
